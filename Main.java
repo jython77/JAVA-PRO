9df0f6813ed76aaa9b688627ab7df7073b7697e9
JIN HAE KIM <141704597+jython77@users.noreply.github.com>
2023-08-08 15:04:14 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("점수를 임력하시요 : ");
         String score_input = sc.next();
         int score = Integer.parseInt(score_input);
         if (score > 95){score = 1;}
